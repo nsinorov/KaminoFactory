e1025341e48b1ba00724e6e0e5d19d5a827e6725
Nikolay Sinorov <nsinorov123@gmail.com>
2022-10-05 18:01:18 +0300

    Add files via upload

---
+++ Kamino Factory/Kamino Factory/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+
+namespace Kamino_Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // sequence lenght 
+            int sequenceLenght = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            int[] DNA = new int[sequenceLenght];
+            int dnaSum = 0;
+            int dnaCount = -1;
+            int dnaStartIndex = -1;
+            int dnaSamples = 0;
+            int sample = 0;
+
+            while (input != "Clone them!")
+            {
+                // Current DNA info
+                sample++;
+
+                int[] currDNA = input.Split("!", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                // Current DNA elements
+                int currCount = 0;
+                int currStartIndex = 0;
+                int currEndIndex = 0;
+                int currDnaSum = 0;
+                bool isCurrDnaBetter = false;
+
+                int count = 0;
+
+                for (int i = 0; i < currDNA.Length; i++)
+                {
+                    if (currDNA[i] != 1)
+                    {
+                        count = 0;
+                        continue;
+                    }
+
+                    count++;
+
+                    if (count > currCount)
+                    {
+                        currCount = count;
+                        currEndIndex = i;
+                    }
+                }
+
+                currStartIndex = currEndIndex - currCount + 1;
+                currDnaSum = currDNA.Sum();
+                // Check current Dna with best Dna
+
+                if (currCount > dnaCount)
+                {
+                    isCurrDnaBetter = true;
+                }
+
+                else if (currCount == dnaCount)
+                {
+                    if (currStartIndex < dnaStartIndex)
+                    {
+                        isCurrDnaBetter = true;
+                    }
+                    else if (currStartIndex == dnaStartIndex)
+                    {
+                        if (currDnaSum > dnaSum)
+                        {
+                            isCurrDnaBetter = true;
+                        }
+                    }                              
+                }
+
+                if (isCurrDnaBetter)
+                {
+                    DNA = currDNA;
+                    dnaCount = currCount;
+                    dnaStartIndex = currStartIndex;
+                    dnaSum = currDnaSum;
+                    dnaSamples = sample;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Best DNA sample {dnaSamples} with sum: {dnaSum}.");
+            Console.WriteLine(String.Join(" ", DNA));
+        }
+    }
+}
